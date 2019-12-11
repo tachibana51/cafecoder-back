0509b0eb70183aca92fbdd38ac274c8e0ee11190
douro <piyopiyo51@gmail.comO>
2019-12-12 01:55:15 +0900

    db

--- database/contest.sql
+++ database/contest.sql
@@ -22,3 +22,20 @@ CREATE TABLE cafecoder.uploads (
     result varchar(8),
     PRIMARY KEY (code_session)
 );
+CREATE TABLE cafecoder.users 
+	(uid varchar(32) NOT NULL,
+	 username varchar(100) NOT NULL,
+	 email varchar(255),
+	 password_hash varchar(64) NOT NULL,
+	 rate int,
+     role varchar(10) NOT NULL,
+	 PRIMARY KEY (uid)
+);
+CREATE TABLE cafecoder.contests
+	(contestid varchar(32) NOT NULL,
+	contestname varchar(32) NOT NULL,
+	indexpath varchar(255),
+	startdate datetime,
+	enddate datetime,
+	PRIMARY KEY (contestid)
+);
