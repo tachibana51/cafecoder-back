119a796b57bc5bae156e76932b76eb4495451457
douro <piyopiyo51@gmail.comO>
2019-12-23 19:10:06 +0900

    fix datetime

--- go/src/values/values.go
+++ go/src/values/values.go
@@ -2,11 +2,11 @@ package values
 
 const (
 	MaxWorker          = 10
-	JudgeHostPort      = "133.130.112.219:8888"
+	JudgeHostPort      = "localhost:8888"
 	FrontHostPort      = "localhost:1234"
 	QueHostPort        = "localhost:4649"
 	QuebackHostPort    = "localhost:5963"
 	FileServerHostPort = "localhost:4444"
-	MySQLDBN           = "cafecoder:aGVsbG93b3JsZAo@/cafecoder"
-	Salt               = "SASLFSLDDSF"
+	MySQLDBN           = "cafecoder:password@/cafecoder"
+	Salt               = "SAAAAALT"
 )
